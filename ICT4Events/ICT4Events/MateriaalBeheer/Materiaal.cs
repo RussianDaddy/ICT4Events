57fb4d958ca2b1874fb2024f2e467b2077db2f1b
Robin Janssen <robinjanssen@gmail.com>
2015-04-05 14:00:41 +0200

    Materiaal/Exemplaat Toegevoegd

---
+++ ICT4Events/ICT4Events/MateriaalBeheer/Materiaal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICT4Events
+{
+    class Materiaal
+    {
+        private string soort;
+        private int borg;
+
+        public Materiaal(string soort, int borg)
+        {
+            this.soort = soort;
+            this.borg = borg;
+        }
+    }
+}
