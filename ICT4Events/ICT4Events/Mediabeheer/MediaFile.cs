951409edf3062fadc2df4aa3d39d6b491eb2bdec
RussianDaddy <sjoerd.weerstra@hotmail.com>
2015-04-08 12:58:07 +0200

    MediaBeheer toegevoegd

---
+++ ICT4Events/ICT4Events/Mediabeheer/MediaFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICT4Events.Mediabeheer
+{
+    class MediaFile
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Path { get; set; }
+        public bool Like { get; set; }
+        public bool Report { get; set; }
+
+        public MediaFile(int id, string type, Categorie categorie, string path, Gebruiker gebruiker)
+        {
+            
+        }
+    }
+}
