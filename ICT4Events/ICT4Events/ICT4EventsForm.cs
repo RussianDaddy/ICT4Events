2ba58bc7482a8965ae0dd3bdabf9ad5101fcc0bf
Robin Janssen <robinjanssen@gmail.com>
2015-04-10 18:36:33 +0200

    Fix It Again! :santa:

--- ICT4Events/ICT4Events/ICT4EventsForm.cs
+++ ICT4Events/ICT4Events/ICT4EventsForm.cs
@@ -1,15 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Web.UI.WebControls;
 using System.Windows.Forms;
-//database
-using Oracle.DataAccess.Client;
 
 namespace ICT4Events
 {
     public partial class ICT4EventsForm : Form
     {
         //private Enum e = new Enum;
+        private List<Mediabeheer.Mediafile> tempSoortList;
+        private string searchstring;
+        Mediabeheer.Mediabeheer mediabeheer;
 
         public ICT4EventsForm()
         {
@@ -76,6 +78,69 @@ namespace ICT4Events
             {
                 Gebruikerbeheer.Registreren(tbGebruikersnaamBeheer.Text, tbNaamBeheer.Text, tbWachtwoordBeheer.Text, 0,"Nee");
             }
+        }
+
+        private void btFilter_Click(object sender, EventArgs e)
+        {
+
+            searchstring = "";
+            LbFeed.Items.Clear();
+            if (chbBericht.Checked)
+            {
+                searchstring += " Bericht";
+                //tempSoortListBericht = mediabeheer.GetSearchedSoort("Bericht");
+            }
+            if (chbBestand.Checked)
+            {
+                searchstring += " Bestand";
+                //tempSoortListBestand = mediabeheer.GetSearchedSoort("Bestand");
+            }
+            if (chbEvent.Checked)
+            {
+                searchstring += " Event";
+                //tempSoortListEvent = mediabeheer.GetSearchedSoort("Event");
+            }
+            if (chbFoto.Checked)
+            {
+                searchstring += " Foto";
+                //tempSoortListFoto = mediabeheer.GetSearchedSoort("Foto");
+            }
+            if (chbVideo.Checked)
+            {
+                searchstring += " Video";
+                //tempSoortListVideo = mediabeheer.GetSearchedSoort("Video");
+            }
+
+            tempSoortList = mediabeheer.GetSearchedSoort(searchstring);
+
+            //Test van methode (later een foreach die alle categoriën waarop gesorteerd is uitleest en vervolgens alle berichten met categorie.categorie die in de lijst staan in een lijst zet en vervolgens al deze berichten in de lbFeed zet
+            foreach (Mediabeheer.Mediafile m in tempSoortList)
+            {
+                LbFeed.Items.Add(m.ToString());
+            }
+
+            tempSoortList = null;
+            mediabeheer.SearchedSoortLijst = null;
+            mediabeheer.SearchedSoortLijst = new List<Mediabeheer.Mediafile>();
+        }
+
+        public void RefreshAll()
+        {
+            LbFeed.Items.Clear();
+            foreach (Mediabeheer.Mediafile m in mediabeheer.GetMediafileLijst)
+            {
+                LbFeed.Items.Add(m.ToString());
+            }
+        }
+
+        private void btShowAll_Click(object sender, EventArgs e)
+        {
+            RefreshAll();
+            chbBericht.Checked = false;
+            chbBestand.Checked = false;
+            chbEvent.Checked = false;
+            chbFoto.Checked = false;
+            chbVideo.Checked = false;
         }
     }
 }
